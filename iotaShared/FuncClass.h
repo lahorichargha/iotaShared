ae0e86b2f65df1455d9c187c07b4650ef5aefd33
Martin Wehlou <martin@mitm.se>
2011-05-20 22:24:01 +0200

    A few trivial functions

---
+++ iotaShared/FuncClass.h
@@ -0,0 +1,19 @@
+//
+//  FuncClass.h
+//  iotaShared
+//
+//  Created by Martin on 2011-05-20.
+//  Copyright 2011 MITM AB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FuncClass : NSObject {
+    
+}
+
+- (int)someNumber;
+- (int)someOtherNumber;
+
+@end
---
+++ iotaShared/FuncClass.m
@@ -0,0 +1,22 @@
+//
+//  FuncClass.m
+//  iotaShared
+//
+//  Created by Martin on 2011-05-20.
+//  Copyright 2011 MITM AB. All rights reserved.
+//
+
+#import "FuncClass.h"
+
+
+@implementation FuncClass
+
+- (int)someNumber {
+    return 55;
+}
+
+- (int)someOtherNumber {
+    return 56;
+}
+
+@end
